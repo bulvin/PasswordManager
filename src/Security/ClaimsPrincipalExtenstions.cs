7a4e04da18f3b7a06925f479720656d81851242c
Jan <janbarn00@gmail.com>
2024-08-01 13:05:23 +0200

    Added ClaimsPrincipalExtensions

---
+++ src/Security/ClaimsPrincipalExtenstions.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+namespace PasswordManager.Security
+{
+    public static class ClaimsPrincipalExtenstions
+    {
+        public static Guid GetUserId(this ClaimsPrincipal claimsPrincipal)
+        {
+            if (!Guid.TryParse(claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier), out var id))
+            {
+                throw new InvalidOperationException("Invalid UserId");
+            }
+
+            return id;
+        }
+    }
+}
